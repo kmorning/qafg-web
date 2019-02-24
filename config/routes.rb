ca661ad84f6f6e05ab9a490f27d2a0e4e9858b56
Kelly Morning <kleemorning@gmail.com>
2019-02-24 15:33:32 -0500

    add 'Meetings Coming Soon'

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  get 'meetings/index'
   get 'home/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'home#index'
+  resources :meetings, only: [:index]
 end
